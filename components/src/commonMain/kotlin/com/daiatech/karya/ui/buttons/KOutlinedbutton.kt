21bd144b69084c8bf9dbd8be5784500473064987
Divyansh Kushwaha <thedroiddiv@gmail.com>
2025-09-30 18:38:29 +0530

    fix package name for spacers

--- components/src/commonMain/kotlin/com/daiatech/karya/ui/buttons/KOutlinedbutton.kt
+++ components/src/commonMain/kotlin/com/daiatech/karya/ui/buttons/KOutlinedbutton.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
+import com.daiatech.karya.ui.theme.Dimens
 import com.daiatech.karya.ui.theme.KaryaTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
