2776177da97793e845c7ad9c2c43230babb62ff2
Divyansh Kushwaha <thedroiddiv@gmail.com>
2025-10-29 14:09:46 +0530

    setup catalogue navigation

--- composeApp/src/commonMain/kotlin/com/daiatech/karya/ui/screens/ComponentScreen.kt
+++ composeApp/src/commonMain/kotlin/com/daiatech/karya/ui/screens/ComponentScreen.kt
@@ -1,39 +1,57 @@
 package com.daiatech.karya.ui.screens
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.daiatech.karya.ui.buttons.IconButtonPreview
+import com.daiatech.karya.ui.Components
 import com.daiatech.karya.ui.buttons.KButtonPreview
-import com.daiatech.karya.ui.components.NavigateUpTopBar
+import com.daiatech.karya.ui.buttons.KIconButtonPreview
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-
-enum class Component(val value: String, val route: String) {
-    BUTTONS("KButtons", "buttons"),
-    ICON_BUTTONS("KIconButtons", "icon_buttons"),
-}
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ComponentScreen(
-    component: Component,
+    component: Components,
+    navigateToSettings: () -> Unit,
     navigateUp: () -> Unit
 ) {
     Scaffold(
         topBar = {
-            NavigateUpTopBar(
-                "${component.value} Catalogue",
-                navigateUp = navigateUp
+            MediumTopAppBar(
+                title = { Text("${component.value} Catalogue") },
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = navigateToSettings) {
+                        Icon(
+                            imageVector = Icons.Filled.Settings,
+                            contentDescription = "Settings"
+                        )
+                    }
+                }
             )
         }
     ) { paddingValues ->
         Box(Modifier.padding(paddingValues)) {
             when (component) {
-                Component.BUTTONS -> KButtonPreview()
-                Component.ICON_BUTTONS -> IconButtonPreview()
+                Components.BUTTON -> KButtonPreview()
+                Components.ICON_BUTTON -> KIconButtonPreview()
             }
         }
     }
@@ -42,11 +60,11 @@ fun ComponentScreen(
 @Preview
 @Composable
 fun ButtonsPreview() {
-    ComponentScreen(Component.BUTTONS) {}
+    ComponentScreen(Components.BUTTON, {}) {}
 }
 
 @Preview
 @Composable
 fun IconButtonsPreview() {
-    ComponentScreen(Component.ICON_BUTTONS) {}
+    ComponentScreen(Components.ICON_BUTTON, {}) {}
 }
---
+++ composeApp/src/commonMain/kotlin/com/daiatech/karya/ui/screens/SettingsScreen.kt
@@ -0,0 +1,35 @@
+package com.daiatech.karya.ui.screens
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreen(
+    navigateUp: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            MediumTopAppBar(
+                title = { Text("Settings") },
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Text("Screen for various screen related configs")
+    }
+}
